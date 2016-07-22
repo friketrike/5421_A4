1f20990e38e901f242de7b83bb8ac8a01caaddf5
friketrike <friketrike@gmail.com>
2016-07-22 01:37:05 -0400

    Started the Canvas deal...

--- Canvas.h
+++ Canvas.h
@@ -2,8 +2,23 @@
 #ifndef CANVAS
 #define CANVAS
 
-class Canvas {
+#include <iostream>
+#include <vector>
 
+class Canvas {
+  private:
+    int m, n;
+    vector<vector<char>> grid;
+
+  public:
+    const int getHeight() const;
+    const int getWidth() const;
+    friend ostream& ostream::operator<<(ostream& ostr, const Canvas& canvas);
+    virtual clear(const char& ch) =0;
+    virtual putChar(const int& c, const int& r, const char& ch) =0;
+    virtual getChar(const int& c, const int& r) =0;
+    decorate() {}
+    virtual validatePos(const int& c, const int& r) =0;
 };
 
 #endif
